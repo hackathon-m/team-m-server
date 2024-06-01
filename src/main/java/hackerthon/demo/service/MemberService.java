d190d554e40d0e787891beaf1ee6cc00bd14e954
yyytir777 <yyytir777@gmail.com>
2024-06-01 21:46:41 +0900

    [Feat] : Member Login 구현

--- src/main/java/hackerthon/demo/service/MemberService.java
+++ src/main/java/hackerthon/demo/service/MemberService.java
@@ -13,18 +13,24 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Optional<Member> login(String serialId) {
+    public Optional<Member> login(String serialId, String nickname) {
         Optional<Member> member = memberRepository.findBySerialId(serialId);
             
         // Member가 비었을 때, 회원가입
         if(member.isEmpty()) {
             Member newMember = Member.builder()
                     .serialId(serialId)
+                    .nickName(nickname)
                     .winRate((double) 0)
                     .build();
 
             memberRepository.save(newMember);
         }
+        return member;
+    }
+
+    public Optional<Member> getInfo(String serialId) {
+        Optional<Member> member = Optional.of(memberRepository.findBySerialId(serialId).orElseThrow());
         return member;
     }
 }
--- src/main/java/hackerthon/demo/web/controller/LoginController.java
+++ src/main/java/hackerthon/demo/web/controller/LoginController.java
@@ -1,11 +1,16 @@
 package hackerthon.demo.web.controller;
 
+import hackerthon.demo.common.dto.MyPageResponseDto;
+import hackerthon.demo.domain.Member;
 import hackerthon.demo.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +19,29 @@ public class LoginController {
 
     private final MemberService memberService;
 
+
+
+    @Operation(summary = "회원가입 및 로그인", description = "")
     @GetMapping("/login")
-    public ResponseEntity<String> login(String serialId) {
-        memberService.login(serialId);
+    public ResponseEntity<String> login(HttpServletRequest request, @RequestParam("nickname") String nickname) {
+        String serialId = request.getHeader("Authorization");
+        memberService.login(serialId, nickname);
         return ResponseEntity.ok("login success");
+    }
+
+
+    @GetMapping("/mypage")
+    public ResponseEntity<MyPageResponseDto> myPage(HttpServletRequest request) throws Exception {
+        String serialId = request.getHeader("Authorization");
+
+        Optional<Member> member = memberService.getInfo(serialId);
+
+        if(member.isPresent()) {
+            Member existedMember = member.get();
+            MyPageResponseDto myPageResponseDto = MyPageResponseDto.convertor(existedMember);
+            return ResponseEntity.ok(myPageResponseDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
